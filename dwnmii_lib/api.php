990bf2bdd77d930a076986ef2343dfb92d927eec
Ivan M. <kurobyte@gmail.com>
2014-11-28 17:35:31 +0100

    Api
    This is the basic scheme of an api to DownloadMii.
    Is only the basic implementation that how will parse the request URL.

---
+++ dwnmii_lib/api.php
@@ -0,0 +1,73 @@
+<?php
+	//DownloadMii (API) v0.1-///
+	/*Documentation
+		This api.php is for parse the request URL and ger the data from the db
+
+		The URL request will be formated like this:
+		To retrieve JSON
+		App list by developer
+		<domain>/api/bydev/[developerId]
+
+		App list by category/sub/other
+		<domain>/api/apps/[category]/[subCategory]/[otherCategory]
+
+		To rate an APP
+		<domain>/api/rate/[appguid]/[rating]
+		[securetoken] - this is better to sendit trought POST data
+
+	*/
+
+	$origKey = "" //Key to verify if the app that is accessing the API is valid
+	$param = explode('/', strtok(getenv('REQUEST_URI'), '?'));
+	$pad = 1; //nº of folders from the root where is located the api folder ej <domain>/<1st>/<2nd>/api
+
+	//get POST parameters
+	$appKey = !empty($_POST['appKey']) ? $_POST['appKey'] : null;
+
+	if ($origKey == $appKey) {
+		switch ($param[3 + $pad]) {
+			case 'bydev':
+				# code...
+				break;
+			
+			case 'apps':
+				switch ($param[4 + $pad]) {
+					case 'TopDownloaded':
+						# code...
+						break;
+
+					case 'StaffPicks':
+						# code...
+						break;
+
+					case 'Applications':
+						if (count($param) > 5) {
+							//get from the applications for the selected categorys/sub categorys
+						}
+						else {
+							//get all the applications ignoring categorys/sub categorys
+						}
+						break;
+
+					case 'Games':
+						if (count($param) > 5) {
+							//get from the games data for the selected categorys/sub categorys
+						}
+						else {
+							//get all the games ignoring categorys/sub categorys
+						}
+						break;
+				}
+				break;
+
+			case 'rate':
+				# code...
+				break;
+		}
+	}
+	else
+	{
+		http_response_code(403);
+		exit();
+	}
+?>
