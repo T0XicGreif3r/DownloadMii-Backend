1924eb572385e25e5a204d1020cbdc876933ed1e
celcodioc <celcodioc@gmail.com>
2014-12-31 18:19:26 +0100

    Update api.php
    /api/bydev/ now works, as well as /api/apps/Applications/ and
    /api/apps/TopDownloadedApps/.

--- api.php
+++ api.php
@@ -21,24 +21,44 @@
 		<domain>/api/banner/[appguid]
 
 	*/
+	
+	function queryResultToJSON($conn, $sql, $name) {
+		//TODO: Category ID -> name, Rating ID -> integer, add error checks
+		
+		$mysqlResult = $conn->query($sql); //Perform query
+		$arr = array();
+		
+		while ($mysqlRow = $mysqlResult->fetch_object()) {
+			array_push($arr, $mysqlRow); //Push all rows to the array
+		}
+		
+		$mysqlResult->close();
+		$jsonResultObj = (object)array($name => $arr); //Create an enclosing object
+		return json_encode($jsonResultObj); //Return JSON
+	}
 
 	$origKey = ""; //Key to verify if the app that is accessing the API is valid
 	$param = explode('/', strtok(getenv('REQUEST_URI'), '?'));
-	$pad = 1; //nº of folders from the root where is located the api folder ej <domain>/<1st>/<2nd>/api
+	$pad = -1; //nº of folders from the root where is located the api folder ej <domain>/<1st>/<2nd>/api
 
 	//get POST parameters
 	$appKey = !empty($_POST['appKey']) ? $_POST['appKey'] : null;
 
 	if ($origKey == $appKey) {
+		$mysqlConn = new mysqli('localhost', 'root', '', 'dwnmii'); //TODO: Use consts from external php file instead of these strings
+		//TODO: Prevent SQL injection
+		
 		switch ($param[3 + $pad]) {
 			case 'bydev':
-				# code...
+				$mysqlQuery = 'SELECT app.* FROM apps app JOIN users usr ON usr.userId = app.creator WHERE usr.nick = "' . $param[4 + $pad] . '"'; //Select rows from apps table by queried developer
+				print(queryResultToJSON($mysqlConn, $mysqlQuery, $param[4 + $pad]));
 				break;
 			
 			case 'apps':
 				switch ($param[4 + $pad]) {
 					case 'TopDownloadedApps':
-						# code...
+						$mysqlQuery = 'SELECT app.* FROM apps app ORDER BY downloads DESC LIMIT 10'; //Select top 10 downloaded apps
+						print(queryResultToJSON($mysqlConn, $mysqlQuery, "TopDownloadedApps"));
 						break;
 
 					case 'TopDownloadedGames':
@@ -50,12 +70,27 @@
 						break;
 
 					case 'Applications':
+						$mysqlQuery = 'SELECT app.* FROM apps app';
+						
+						//Category query appending
 						if (count($param) > 5) {
-							//get from the applications for the selected categorys/sub categorys
+							$mysqlQueryEnd = ' WHERE maincat.name = "' . $param[5 + $pad] . '"';
+							$mysqlQuery .= ' JOIN categories maincat ON maincat.categoryId = app.category';
+							
+							if (count($param) > 6) {
+								$mysqlQueryEnd .= ' AND subcat.name = "' . $param[6 + $pad] . '"';
+								$mysqlQuery .= ' JOIN categories subcat ON subcat.categoryId = app.subCategory';
+							
+								if (count($param) > 7) {
+									$mysqlQueryEnd .= ' AND othercat.name = "' . $param[7 + $pad] . '"';
+									$mysqlQuery .= ' JOIN categories othercat ON othercat.categoryId = app.otherCategory';
+								}
+							}
+							
+							$mysqlQuery .= $mysqlQueryEnd;
 						}
-						else {
-							//get all the applications ignoring categorys/sub categorys
-						}
+						
+						print(queryResultToJSON($mysqlConn, $mysqlQuery, "Apps"));
 						break;
 
 					case 'Games':
@@ -87,6 +122,7 @@
 				}
 				break;
 		}
+		$mysqlConn->close();
 	}
 	else
 	{
