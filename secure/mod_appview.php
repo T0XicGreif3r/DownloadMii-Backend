6cb445b18f4d389d0aa8cf35dd297a689f1414c8
Filiph Sandström <filfat@hotmail.se>
2015-01-08 23:35:04 +0100

    todo

--- secure/mod_appview.php
+++ secure/mod_appview.php
@@ -6,5 +6,13 @@
 	
 	$_SESSION['mod_token'] = uniqid(mt_rand(), true); //Generate token for moderator action
 	
+	if (isset($_GET['guid'], $_GET['token'], $myappsToken) && md5($myappsToken) === $_GET['token']) {
+			$matchingApps = getArrayFromSQLQuery($mysqlConn, 'SELECT app.*, appver.number AS version FROM apps app
+																LEFT JOIN appversions appver ON appver.versionId = app.version
+																WHERE app.guid = ? AND app.publisher = ? LIMIT 1', 'ss', [$_GET['guid'], $_SESSION['user_id']]); //Get app with user/GUID combination
+	
+	printAndExitIfTrue(count($matchingApps) != 1, 'Invalid app GUID.'); //Check if there is one app matching attempted GUID/user combination
+	$currentApp = $matchingApps[0];
+	
 	echo 'ToDo<br />';
 ?>
