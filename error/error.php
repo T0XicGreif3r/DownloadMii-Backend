c47cd05d610c0ec80fc963bd8f70b9cc19961be5
Filiph Sandström <filfat@hotmail.se>
2015-01-08 15:22:35 +0100

    Todo

--- error/error.php
+++ error/error.php
@@ -1,3 +1,65 @@
+<html>
+	<head>
+		<title>DownloadMii User CP</title>
+		<link href="/css/bootstrap.css" rel="stylesheet"/>
+		<link href="/css/mainStruct.css" rel="stylesheet"/>
+		<style>
+			header{
+				margin-bottom: 75px;
+			}
+		</style>
+	</head>
+	<body>
+		<header>
+			<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+			  <div class="container-fluid">
+				<div class="navbar-header">
+				  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-main">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				  </button>
+				  <a class="navbar-brand" href="/">DownloadMii</a>
+				</div>
+				<div class="collapse navbar-collapse" id="navbar-collapse-main">
+				  <ul class="nav navbar-nav navbar-right">
+					<li><a href="/">HOME</a></li>
+					<!--li><a data-scroll href="#ABOUTwp">ABOUT</a></li-->
+					<li><a data-scroll href="/#DOWNLOADwp">DOWNLOAD</a></li>
+					<li><a href="/donate">DONATE</a></li>
+					<li><a href="/secure/myapps.php">USER CP</a></li> <!-- ToDo: redirect to user CP instead of "my apps" list -->
+				  </ul>
+				</div>
+			  </div>
+			</nav>
+		</header>
+		<div class="text-center">
+			<?php
+			$requestUri = strtok(getenv('REQUEST_URI'), '?');
+			$param = explode('/error', rtrim(substr($requestUri, strlen('/error.php/')), '/')); //All URL "directories" after /api/ -> array
+			$topLevelRequest = $param[0];
+			switch ($topLevelRequest) {
+				case '404':
+			?>
+				<h1>Error 404</h1>
+				<h4>The requested page cannot be found!</h4>
+			<?php
+				break;
+				case '500':
+			?>
+				<h1>Error 500</h1>
+				<h4>Server error!</h4>
+			<?php
+				break;
+				default:
+			?>
+				<h1>Error</h1>
+				<h4>Unknown error!</h4>
+			<?php
+			}
+			?>
+		</div>
 <?php
-echo 'todo';
+	require_once('../common/ucpfooter.php');
 ?>
