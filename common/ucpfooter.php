2e83358d8cbb79b10bcfb82f9696023f376d4c60
Filiph Sandström <filfat@hotmail.se>
2015-01-08 22:49:15 +0100

    Update ucpfooter.php

--- common/ucpfooter.php
+++ common/ucpfooter.php
@@ -62,7 +62,6 @@
 		</footer>
 		<!-- SCRIPTS  -->
 		<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-		<script src="/js/jquery.scrollUp.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 		<script src="/js/smooth-scroll.js"></script>
 		<script>
