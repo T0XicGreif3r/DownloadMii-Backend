090533c60f100e3cf83f7e750598ddea64cedae6
Filiph Sandström <filfat@hotmail.se>
2015-01-14 15:34:21 +0100

    added meta.php

---
+++ common/meta.php
@@ -0,0 +1,58 @@
+<!-- SEO -->
+<meta name="description" content="DownloadMii is an online marketplace for Homebrew applications. Its also 100% free fo charge!" />
+<meta name="keywords" content="Nintendo 3DS Homebrew, Homebrew Browser, 3ds, filfat, Wii U Homebrew, DownloadMii" />
+<meta name="author" CONTENT="DownloadMii Team">
+<meta name="google-site-verification" content="oUzBcLbkKoA1gb5G8ZWgzzLR_LmIMFa0F8gsCH39LRc" />
+<meta name="msvalidate.01" content="-" />
+<meta charset="utf-8" />
+
+<!-- METADATA -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<link rel="shortcut icon" type="image/ico" href="https://www.downloadmii.com/favicon.ico" />
+
+<!-- STYLESHEETS -->
+<link href="/css/bootstrap.css" rel="stylesheet"/>
+<link href="/css/mainStruct.css" rel="stylesheet"/>
+
+<!-- IN CASE OF IE 9 -->
+<!--[if lt IE 9]> 
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+<!-- PLATFORM SPECIFIC -->
+<!-- MICROSOFT -->
+<meta name="application-name" content="DownloadMii" />
+<link rel="dns-prefetch" href="http://filfatstudios.com/"/>
+<link rel="prerender" href="http://filfatstudios.com/" />
+
+<!-- IE SITE PIN -->
+<meta name="msapplication-starturl" content="http://www.downloadmii.com" />
+<meta name="msapplication-navbutton-color" content="#1009FFF" />
+<meta name="msapplication-window" content="width=1024;height=768" />
+<meta name="msapplication-tooltip" content="Visit DownloadMii" />
+<meta content="name=Home;action-uri=./index.html;icon-uri=./favicon.ico" name="msapplication-task" />
+<meta content="name=User CP;action-uri=./secure/myapps.php;icon-uri=./favicon.ico" name="msapplication-task" />
+
+
+<!-- LIVE TILE -->
+<meta name="msapplication-TileColor" content="#009FFF" />
+<meta name="msapplication-square70x70logo" content="/img/LiveTiles/smalltile.png" />
+<meta name="msapplication-square150x150logo" content="/img/LiveTiles/mediumtile.png" />
+<meta name="msapplication-wide310x150logo" content="/img/LiveTiles/widetile.png" />
+<meta name="msapplication-square310x310logo" content="/img/LiveTiles/largetile.png" />
+
+<!-- APPLE -->
+<link rel="apple-touch-icon" href="/img/Apple/touch-icon-iphone.png" /> 
+<link rel="apple-touch-icon" sizes="76x76" href="/img/Apple/touch-icon-ipad.png" /> 
+<link rel="apple-touch-icon" sizes="120x120" href="/img/Apple/touch-icon-iphone-retina.png" />
+<link rel="apple-touch-icon" sizes="152x152" href="/img/Apple/touch-icon-ipad-retina.png" />
+<link rel="apple-touch-startup-image" href="/img/Apple/startup.png" />
+<meta name="apple-mobile-web-app-status-bar-style" content="white" />
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-title" content="DownloadMii"/>
+<link rel="apple-touch-startup-image" href="/img/Apple/startup.png">
+
+<!-- ANDROID -->
+<link rel="icon" sizes="128x128" href="/img/Apple/touch-icon-iphone-retina.png" />
--- donate/index.php
+++ donate/index.php
@@ -1,9 +1,15 @@
 <html>
 	<head>
-		<title>DownloadMii User CP</title>
-		<link href="/css/bootstrap.css" rel="stylesheet"/>
-		<link href="/css/mainStruct.css" rel="stylesheet"/>
+		<?php
+			require_once('../common/meta.php');
+		?>
+		<title>DownloadMii - Donate</title>
 		<style>
+			body{
+				overflow-x:hidden;
+				-ms-overflow-x: hidden;
+				max-width: 100%;
+			}
 			header{
 				margin-bottom: 75px;
 			}
@@ -36,16 +42,32 @@
 		</header>
 		<div class="text-center">
 			<h1>Donate</h1>
-			<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-				<input type="hidden" name="cmd" value="_donations">
-				<input type="hidden" name="business" value="filfat@hotmail.se">
-				<input type="hidden" name="lc" value="US">
-				<input type="hidden" name="no_note" value="0">
-				<input type="hidden" name="currency_code" value="USD">
-				<input type="hidden" name="bn" value="PP-DonationsBF:btn_donateCC_LG.gif:NonHostedGuest">
-				<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-				<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-			</form>
+			<br />
+			<div class="row text-center">
+			  <div class="row">
+				  <div class="col-sm-5 col-xs-12 well clearfix" style="background: #25A4D6;box-shadow: 0 4px 2px -2px rgba(0,0,0,0.4);border:0;">
+					<h2 class="font-white">Girtpay</h2>
+					<h4 class="font-white">(recommended)<h4>
+					<script data-gratipay-username="filfat" src="//grtp.co/v1.js"></script>
+				  </div>
+				  <div class="col-sm-2 col-xs-0"></div>
+				  <div class="col-sm-5 col-xs-12 well clearfix" style="background: #25A4D6;box-shadow: 0 4px 2px -2px rgba(0,0,0,0.4);border:0;">
+					<h2 class="font-white">PayPal</h2>
+					<br />
+					<br />
+					<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+						<input type="hidden" name="cmd" value="_donations">
+						<input type="hidden" name="business" value="filfat@hotmail.se">
+						<input type="hidden" name="lc" value="US">
+						<input type="hidden" name="no_note" value="0">
+						<input type="hidden" name="currency_code" value="USD">
+						<input type="hidden" name="bn" value="PP-DonationsBF:btn_donateCC_LG.gif:NonHostedGuest">
+						<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+						<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+					</form>
+				  </div>
+			  </div>
+			</div>
 			<p>Keeping the server up costs quite a bit, so does also the time we spend<br />
 			   developing this application. If you like DownloadMii consider donating<br />
 			   to help us keep DownloadMii alive! Thanks :)</p>
