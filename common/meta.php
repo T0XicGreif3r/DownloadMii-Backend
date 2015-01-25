355e04548fba7a05bc22f796406570efd6cc2925
Filiph Sandström <filfat@hotmail.se>
2015-01-25 17:13:15 +0100

    Fixed #13

--- common/meta.php
+++ common/meta.php
@@ -58,3 +58,14 @@
 
 <!-- ANDROID -->
 <link rel="icon" sizes="128x128" href="/img/Apple/touch-icon-iphone-retina.png" />
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-36429627-11', 'auto');
+  ga('send', 'pageview');
+
+</script>
