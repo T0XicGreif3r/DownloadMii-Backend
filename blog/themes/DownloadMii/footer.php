16a9e28a9a041fb49acae8794f02fcf5d7f76853
Filiph Sandström <filfat@hotmail.se>
2015-02-02 08:52:02 +0100

    Added rss

--- blog/themes/DownloadMii/footer.php
+++ blog/themes/DownloadMii/footer.php
@@ -1,7 +1,7 @@
 		<div class="wrap">
 	            <footer>
 					<div class="content">
-						&copy;filfat Studios 2014-2015; Website designed by <a href="http://www.filfatstudios.com">filfat Studios</a>; <a href="<?php echo base_url('admin'); ?>" title="Administer your site!">Admin area</a>
+						&copy;filfat Studios 2014-2015; Website designed by <a href="http://www.filfatstudios.com">filfat Studios</a>; <a href="<?php echo rss_url(); ?>">RSS</a>; <a href="<?php echo base_url('admin'); ?>" title="Administer your site!">Admin area</a>
 					</div>
 				</footer>
 
