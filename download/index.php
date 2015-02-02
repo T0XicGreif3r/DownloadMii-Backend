b0dd652a0720a4704aad75858ffcee2673cbd7a7
Filiph Sandström <filfat@hotmail.se>
2015-02-02 21:16:37 +0100

    Update index.php

--- download/index.php
+++ download/index.php
@@ -43,13 +43,13 @@
 				<br />
 				<div class="row text-center">
 				  <div class="row">
-					  <div class="col-sm-5 col-xs-12 well clearfix" style="background: #25A4D6;box-shadow: 0 4px 2px -2px rgba(0,0,0,0.4);border:0;">
+					  <div id="release" class="col-sm-5 col-xs-12 well clearfix" style="background: #25A4D6;box-shadow: 0 4px 2px -2px rgba(0,0,0,0.4);border:0;">
 					  	<h2 class="font-white">Latest Release</h2>
-						<h4 class="font-white">1.0.5.4</h4>
-						<p><a class="btn btn-lg btn-flat" href="https://github.com/DownloadMii/DownloadMii/releases/download/1.0.5.4/1054.zip" role="button">Download</a></p>
+						<h4 class="font-white">1.0.5.6</h4>
+						<p><a class="btn btn-lg btn-flat" href="https://github.com/DownloadMii/DownloadMii/releases/download/1.0.5.6/1056.zip" role="button">Download</a></p>
 					  </div>
 					  <div class="col-sm-2 col-xs-0"></div>
-					  <div class="col-sm-5 col-xs-12 well clearfix" style="background: #25A4D6;box-shadow: 0 4px 2px -2px rgba(0,0,0,0.4);border:0;">
+					  <div id="beta" class="col-sm-5 col-xs-12 well clearfix" style="background: #25A4D6;box-shadow: 0 4px 2px -2px rgba(0,0,0,0.4);border:0;">
 					  	<h2 class="font-white">Latest Beta</h2>
 						<h4 class="font-white">Coming Soon!</h4>
 						<p><a class="btn btn-lg btn-flat disabled" href="/download/#beta" role="button">Download</a></p>
