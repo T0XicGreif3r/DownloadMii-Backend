51a330bdc30eb4cf31068d0e2617bcfb46dd77d6
Filiph Sandström <filfat@hotmail.se>
2015-02-07 18:11:00 +0100

    Update article.php

--- blog/themes/DownloadMii/article.php
+++ blog/themes/DownloadMii/article.php
@@ -34,53 +34,18 @@
 				<a href="https://www.downloadmii.com">Return to DownloadMii's homepage </a>
 			</article>
 		</section>
-		<?php if(comments_open()): ?>
 		<section class="comments">
-			<?php if(has_comments()): ?>
-			<h3>Comments</h3>
-			<ul class="commentlist">
-				<?php $i = 0; while(comments()): $i++; ?>
-				<li class="comment" id="comment-<?php echo comment_id(); ?>">
-					<div class="wrap">
-						<h2><?php echo comment_name(); ?></h2>
-						<time><?php echo relative_time(comment_time()); ?></time>
-
-						<div class="content">
-							<?php echo comment_text(); ?>
-						</div>
-
-						<span class="counter"><?php echo $i; ?></span>
-					</div>
-				</li>
-				<?php endwhile; ?>
-			</ul>
-			<?php endif; ?>
+			  <div id="disqus_thread"></div>
+			    <script type="text/javascript">
+			        var disqus_shortname = 'downloadmii'; // required: replace example with your forum shortname
 			
-			<h3>Leave a comment</h3>
-			<form id="comment" class="commentform wrap" method="post" action="<?php echo comment_form_url(); ?>#comment">
-				<?php echo comment_form_notifications(); ?>
-
-				<p class="name">
-					<label for="name">Your name:</label>
-					<?php echo comment_form_input_name('placeholder="Your name"'); ?>
-				</p>
-
-				<p class="email">
-					<label for="email">Your email address:</label>
-					<?php echo comment_form_input_email('placeholder="Your email (won’t be published)"'); ?>
-				</p>
-
-				<p class="textarea">
-					<label for="text">Your comment:</label>
-					<?php echo comment_form_input_text('placeholder="Your comment"'); ?>
-				</p>
-
-				<p class="submit">
-					<?php echo comment_form_button(); ?>
-				</p>
-			</form>
-
+			        /* * * DON'T EDIT BELOW THIS LINE * * */
+			        (function() {
+			            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+			            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+			            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+			        })();
+			    </script>
 		</section>
-		<?php endif; ?>
 	</div>
 <?php theme_include('footer'); ?>
