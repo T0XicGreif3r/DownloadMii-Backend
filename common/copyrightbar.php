38a1645deeb66cde40ef826f56366f3b38e59498
Filiph Sandström <filfat@hotmail.se>
2015-02-10 13:38:20 +0100

    Update copyrightbar.php

--- common/copyrightbar.php
+++ common/copyrightbar.php
@@ -1,3 +1,8 @@
+<?php
+	/*
+		DownloadMii Copyright Bar
+	*/
+?>
 <footer>
   <div class="content">
     &copy;Filiph Sandstr&ouml;m 2014-2015; Website designed by <a href="http://www.filfatstudios.com">Filiph Sandstr&ouml;m</a>; (Owners retain copyright to their respective works)
