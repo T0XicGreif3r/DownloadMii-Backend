ee17192f7431e65a44cc1143f4ae052fffbf34bb
Filiph Sandström <filfat@hotmail.se>
2015-02-10 13:40:32 +0100

    Update footer.php

--- blog/themes/DownloadMii/footer.php
+++ blog/themes/DownloadMii/footer.php
@@ -1,10 +1,9 @@
 		<div class="wrap">
 	            <footer>
-					<div class="content">
-						&copy;filfat Studios 2014-2015; Website designed by <a href="http://www.filfatstudios.com">filfat Studios</a>; <a href="<?php echo rss_url(); ?>">RSS</a>; <a href="<?php echo base_url('admin'); ?>" title="Administer your site!">Admin area</a>
-					</div>
-				</footer>
-
+			<div class="content">
+				&copy;Filiph Sandstr&ouml;m 2014-2015; Website designed by <a href="http://www.filfatstudios.com">fFiliph Sandstr&ouml;m</a>; <a href="<?php echo rss_url(); ?>">RSS</a>; <a href="<?php echo base_url('admin'); ?>" title="Administer your site!">Admin area</a>
+			</div>
+		    </footer>
 	        </div>
         </div>
     </body>
