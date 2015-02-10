d8c324fe02155e9d92b266061ce958686ec7e55d
Filiph Sandström <filfat@hotmail.se>
2015-02-10 22:32:10 +0100

    Working on the About Uss page

--- about/index.php
+++ about/index.php
@@ -7,8 +7,51 @@
 	?>
 </head>
 <body>
-	<h1 class="text-center">About Us</h1>
-	ToDo
+	<h1 class="animated bounceInDown text-center">About Us</h1>
+	<br />
+	<div class="animated bounceInLeft text-center" style="font-size: 20px;">
+		<h2>About</h2>
+		DownloadMii is an online marketplace for homebrew applications that runs nativly on the supported platform(s).
+		Development started November 2014 by <a href="http://filfatstudios.com">Filiph Sandstr&ouml;m</a> after users requested a "app store" for the Nintendo 3DS.
+		DownloadMii is currently in early development which means that bug can, nay; may occur, You have been warned.
+		<h2>Credits</h2>
+		<h3>Website Frontend<h3>
+			<li><a href="http://filfatstudios.com">Filiph Sandstr&ouml;m</a> (Main Developer)</li>
+			<li>celcodioc (Developer)</li>
+		<h3>3DS App<h3>
+			<li><a href="http://filfatstudios.com">Filiph Sandstr&ouml;m</a> (Main Developer)</li>
+			<li>celcodioc (Minor Contributions)</li>
+		<h3>Website Backend<h3>
+			<li>celcodioc (Main Developer)</li>
+			<li><a href="http://filfatstudios.com">Filiph Sandstr&ouml;m</a> (Minor Contributions)</li>
+			<li>Kurobyte (Minor Contributions)</li>
+		<h3>Thanks To<h3>
+			<li><a href="https://github.com/smealum/ctrulib">The CTRULIB Developers</a> (3DS Library)</li>
+			<li><a href="https://gbatemp.net">GBATEMP.NET</a></li>
+			<li><a href="http://daneden.github.io/animate.css/">animate.css</a> (CSS Animation Library)</li>
+			<li>Bootstrap (Website Framework)</li>
+			<li>jQuery (JavaScript Library)</li>
+			<li>#3dsdev</li>
+			<li>All the Contributors that didn't make it to this list! :)</li>
+	</div>
 <?php
 	require_once('../common/uifooter.php');
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
