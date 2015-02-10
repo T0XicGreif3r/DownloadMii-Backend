6b6c7ed7cc02dd833282e091869451c832ad5fb2
Filiph Sandström <filfat@hotmail.se>
2015-02-10 13:24:13 +0100

    Create copyrightbar.php

---
+++ common/copyrightbar.php
@@ -0,0 +1,5 @@
+<footer>
+  <div class="content">
+    &copy;Filiph Sandstr&ouml;m 2014-2015; Website designed by <a href="http://www.filfatstudios.com">Filiph Sandstr&ouml;m</a>; (Owners retain copyright to their respective works)
+  </div>
+</footer>
