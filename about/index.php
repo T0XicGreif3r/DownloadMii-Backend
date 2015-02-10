fd90b1a89d53e75eac78876df8300c202a99b14f
Filiph Sandström <filfat@hotmail.se>
2015-02-10 13:43:16 +0100

    Create index.php

---
+++ about/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head runat="server">
+	<?php
+		require_once('../common/meta.php');
+	?>
+    <title>DownloadMii - Download</title>
+</head>
+<body>
+	<h1 class="text-center">About Us</h1>
+	ToDo
+<?php
+	require_once('../common/uifooter.php');
+?>
