84266b3f151d68f32a3101143bbffb3d0fe19134
Filiph Sandström <filfat@hotmail.se>
2015-02-12 10:34:59 +0100

    Update index.php

--- about/index.php
+++ about/index.php
@@ -1,45 +1,40 @@
-<!DOCTYPE html>
-<html>
-<head runat="server">
-	<?php
-		$title = 'About Us';
-		require_once('../common/uiheader.php');
-	?>
-</head>
-<body>
-	<h1 class="animated bounceInDown text-center">About Us</h1>
+<?php
+	$title = 'About Us';
+	require_once('../common/uiheader.php');
+?>
+<h1 class="animated bounceInDown text-center">About Us</h1>
+<br />
+<div class="animated bounceInLeft text-center" style="font-size: 20px;">
+	<h2>About</h2>
+	DownloadMii is an online marketplace for homebrew applications that runs nativly on the supported platform(s).
+	Development started November 2014 by <a href="http://filfatstudios.com">Filiph Sandstr&ouml;m</a> after users requested a "app store" for the Nintendo 3DS.
+	DownloadMii is currently in early development which means that bug can, nay; may occur, You have been warned.
+	If you have any futher questions make sure to contact us <a href="/contact/">here</a>.
 	<br />
-	<div class="animated bounceInLeft text-center" style="font-size: 20px;">
-		<h2>About</h2>
-		DownloadMii is an online marketplace for homebrew applications that runs nativly on the supported platform(s).
-		Development started November 2014 by <a href="http://filfatstudios.com">Filiph Sandstr&ouml;m</a> after users requested a "app store" for the Nintendo 3DS.
-		DownloadMii is currently in early development which means that bug can, nay; may occur, You have been warned.
-		If you have any futher questions make sure to contact us <a href="/contact/">here</a>.
-		<br />
-		<h2>Credits</h2>
-		<h3>Website Frontend<h3>
-			<li><a href="http://filfatstudios.com">Filiph Sandstr&ouml;m</a> (Main Developer)</li>
-			<li>celcodioc (Developer)</li>
-		<h3>3DS App<h3>
-			<li><a href="http://filfatstudios.com">Filiph Sandstr&ouml;m</a> (Main Developer)</li>
-			<li>celcodioc (Minor Contributions)</li>
-		<h3>Website Backend<h3>
-			<li>celcodioc (Main Developer)</li>
-			<li><a href="http://filfatstudios.com">Filiph Sandstr&ouml;m</a> (Minor Contributions)</li>
-			<li>Kurobyte (Minor Contributions)</li>
-		<h3>Thanks To<h3>
-			<li><a href="https://github.com/smealum/ctrulib">The CTRULIB Developers</a> (3DS Library)</li>
-			<li><a href="https://gbatemp.net">GBATEMP.NET</a></li>
-			<li><a href="http://daneden.github.io/animate.css/">animate.css</a> (CSS Animation Library)</li>
-			<li>Bootstrap (Website Framework)</li>
-			<li>jQuery (JavaScript Library)</li>
-			<li>#3dsdev</li>
-			<li>All the Contributors that didn't make it to this list! :)</li>
-		<br />
-		<h2>Legal</h2>
-		DownloadMii is licensed under a modified version of GNU that you can find <a href="https://raw.githubusercontent.com/DownloadMii/DownloadMii/master/LICENSE">here</a>.
-		By using this service you agree to <a href="https://www.downloadmii.com/legal/terms/">these</a> terms.
-	</div>
+	<h2>Credits</h2>
+	<h3>Website Frontend<h3>
+		<li><a href="http://filfatstudios.com">Filiph Sandstr&ouml;m</a> (Main Developer)</li>
+		<li>celcodioc (Developer)</li>
+	<h3>3DS App<h3>
+		<li><a href="http://filfatstudios.com">Filiph Sandstr&ouml;m</a> (Main Developer)</li>
+		<li>celcodioc (Minor Contributions)</li>
+	<h3>Website Backend<h3>
+		<li>celcodioc (Main Developer)</li>
+		<li><a href="http://filfatstudios.com">Filiph Sandstr&ouml;m</a> (Minor Contributions)</li>
+		<li>Kurobyte (Minor Contributions)</li>
+	<h3>Thanks To<h3>
+		<li><a href="https://github.com/smealum/ctrulib">The CTRULIB Developers</a> (3DS Library)</li>
+		<li><a href="https://gbatemp.net">GBATEMP.NET</a></li>
+		<li><a href="http://daneden.github.io/animate.css/">animate.css</a> (CSS Animation Library)</li>
+		<li>Bootstrap (Website Framework)</li>
+		<li>jQuery (JavaScript Library)</li>
+		<li>#3dsdev</li>
+		<li>All the Contributors that didn't make it to this list! :)</li>
+	<br />
+	<h2>Legal</h2>
+	DownloadMii is licensed under a modified version of GNU that you can find <a href="https://raw.githubusercontent.com/DownloadMii/DownloadMii/master/LICENSE">here</a>.
+	By using this service you agree to <a href="https://www.downloadmii.com/legal/terms/">these</a> terms.
+</div>
 <?php
 	require_once('../common/uifooter.php');
 ?>
