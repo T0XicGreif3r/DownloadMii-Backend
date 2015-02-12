801e0665cbf751d8045725febc98f84d28b3f555
Filiph Sandström <filfat@hotmail.se>
2015-02-12 19:28:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,26 +2,24 @@
 	$title = 'Home';
 	require_once('/common/uiheader.php');
 ?>
-  <div class="container">
-	<div class="row">
-	  <div class="col-sm-12 text-center">
-		<div class="animated bounceInDown"><img class="Product" src="img/Logo.png" alt="DownloadMii logo" /></div>
-		<p class="lead">
-			DownloadMii is an online marketplace for Nintendo 3DS homebrew applications and its 100% free of charge for the end-user. It is currently under development, and the GitHub project can be found <a href="https://github.com/DownloadMii/DownloadMii">here</a>!<br />
-			You can read about the latest news, 3DS news and app releases/showcases <a href="https://www.downloadmii.com/blog/">here</a>.<br />
-			To use DownloadMii, you will need a way to run .3dsx files on your Nintendo 3DS. The .cia format for the main application in the future,<br />
-			However DownloadMii will still only support downloading .3dsx applications.<br />
-			<br />
-			Are you a Developer? Remember:<br />
-			We <span class="glyphicon glyphicon-heart" aria-hidden="true" style="color: red;"></span> Developers!
-		</p>
-		<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/DownloadMii" data-widget-id="565828916062212097">Tweets av @DownloadMii</a>
-		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-		<h1 class="SEO">DownloadMii</h1>
-		<h2 class="SEO">Download free 3DS homebrew applications</h2>
-	  </div>
-	</div>
+<div class="row">
+  <div class="col-sm-12 text-center">
+	<div class="animated bounceInDown"><img class="Product" src="img/Logo.png" alt="DownloadMii logo" /></div>
+	<p class="lead">
+		DownloadMii is an online marketplace for Nintendo 3DS homebrew applications and its 100% free of charge for the end-user. It is currently under development, and the GitHub project can be found <a href="https://github.com/DownloadMii/DownloadMii">here</a>!<br />
+		You can read about the latest news, 3DS news and app releases/showcases <a href="https://www.downloadmii.com/blog/">here</a>.<br />
+		To use DownloadMii, you will need a way to run .3dsx files on your Nintendo 3DS. The .cia format for the main application in the future,<br />
+		However DownloadMii will still only support downloading .3dsx applications.<br />
+		<br />
+		Are you a Developer? Remember:<br />
+		We <span class="glyphicon glyphicon-heart" aria-hidden="true" style="color: red;"></span> Developers!
+	</p>
+	<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/DownloadMii" data-widget-id="565828916062212097">Tweets av @DownloadMii</a>
+	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+	<h1 class="SEO">DownloadMii</h1>
+	<h2 class="SEO">Download free 3DS homebrew applications</h2>
   </div>
+</div>
 <?php
 	require_once('/common/uifooter.php');
 ?>
