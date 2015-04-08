9d2bc499904307205bb66ecf0f11d6535e0cb68f
Filiph Sandström <filfat@hotmail.se>
2015-04-08 15:56:17 +0200

    SEO

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
   <div class="col-sm-12 text-center">
 	<div class="animated bounceInDown"><img class="Product" src="img/Logo.png" alt="DownloadMii logo" /></div>
 	<p class="lead">
-		DownloadMii is an online marketplace for Nintendo 3DS homebrew applications and its 100% free of charge for the end-user. It is currently under development, and the GitHub project can be found <a href="https://github.com/DownloadMii/DownloadMii">here</a>!<br />
+		DownloadMii is an online marketplace for homebrew applications on the Nintendo 3DS and the Nintendo Wii U (coming somethime in the future) and its 100% free of charge for the end-user. It is currently under development, and the GitHub project can be found <a href="https://github.com/DownloadMii/DownloadMii">here</a>!<br />
 		You can read about the latest news, 3DS news and app releases/showcases <a href="https://www.downloadmii.com/blog/">here</a>.<br />
-		To use DownloadMii, you will need a way to run .3dsx files on your Nintendo 3DS. The .cia format for the main application in the future,<br />
-		However DownloadMii will still only support downloading .3dsx applications.<br />
+		To use DownloadMii, you will need a way to run homebrew applications on your Nintendo 3DS OR Nintendo Wii U.<br />
+
 		<br />
 		Are you a Developer? Remember:<br />
 		We <span class="glyphicon glyphicon-heart" aria-hidden="true" style="color: red;"></span> Developers!
@@ -18,6 +18,7 @@
 	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 	<h1 class="SEO">DownloadMii</h1>
 	<h2 class="SEO">Download free 3DS homebrew applications</h2>
+	<h2 class="SEO">Download free Wii U homebrew applications</h2>
   </div>
 </div>
 <?php
